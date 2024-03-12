71d15a5fa3d8e2237cea3dbaacb36143298bcba6
Reyhan Asta <astareyhan@gmail.com>
2024-03-12 15:40:51 +0700

    Rentang Umur

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -10,12 +10,12 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
+use App\Http\Controllers\ReportController;
 
 class ReportExport implements FromCollection, WithMapping, WithHeadings
 {
-
-
-    /**
+  
+ /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
@@ -23,39 +23,41 @@ class ReportExport implements FromCollection, WithMapping, WithHeadings
         // Dapatkan tanggal awal dan akhir bulan ini
         $firstDayofMonth = Carbon::now()->startOfMonth();
         $lastDayofMonth = Carbon::now()->endOfMonth();
-        $data = Register::whereBetween('tgl_registrasi', [$firstDayofMonth, $lastDayofMonth])->get();
-        // $data = Register::latest()->paginate(3);        
+        $data = Register::whereBetween('tgl_registrasi', [$firstDayofMonth, $lastDayofMonth])
+        ->orderBy('jam_reg', 'asc')
+        ->get();
         return $data;
     }
 
 
     public function map($data): array
     {
+        $controllerInstance = new ReportController();
+        $rentangUmur = $controllerInstance->calculateAgeRange($data->umurdaftar);
+       
         $result = [
             $data->patient->nm_pasien,
             $data->no_rkm_medis,
             $data->umurdaftar . " " . $data->sttsumur,
-            "-",
-            $data->tgl_registrasi,
+            $rentangUmur,
+            date('d-m-Y', strtotime($data->tgl_registrasi)),
             $data->patient->jk,
             $data->caraBayar->png_jawab,
-            $data->status_poli,
-            "asal_pasien",
+            $data->stts_daftar,
+            $data->asal_pasien ?? 'Datang Sendiri',
             $data->poli->nm_poli,
             $data->doctor->nm_dokter,
             $data->status_lanjut,
         ];
-
         $diseasesByNoRawat = optional($data->disease)->groupBy('pivot.no_rawat') ?? collect();
+        // Sort the diseases in descending order by no_rawat
+        // $diseasesByNoRawat = $diseasesByNoRawat->sortByDesc->first()->first();
         foreach ($diseasesByNoRawat as $noRawat => $diseases) {
             // Add your condition here
-           
             if ($noRawat != $data->no_rawat) {
                 break;
             }
-        
             $result[] = $diseases->isEmpty() ? "-" : $diseases->pluck('nm_penyakit')->implode(', ');
-            dd($result);
         }
 
         return [$result]; // Wrap $result dalam array agar setiap diagnosa terpisah
--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -31,6 +31,27 @@ class ReportController extends Controller
         
         return Excel::download(new ReportExport, 'users.xlsx');
     }
+
+    public function calculateAgeRange($umur): string
+    {
+        if ($umur >= 0 && $umur <= 4) {
+            return "0-4";
+        } elseif ($umur >= 5 && $umur <= 9) {
+            return "5-9";
+        } elseif ($umur >= 10 && $umur <= 14) {
+            return "10-14";
+        } elseif ($umur >= 15 && $umur <= 24) {
+            return "15-24";
+        } elseif ($umur >= 25 && $umur <= 44) {
+            return "25-44";
+        } elseif ($umur >= 45 && $umur <= 64) {
+            return "45-64";
+        } elseif ($umur >= 65) {
+            return "65>";
+        }
+
+        return "-";
+    }
     public function create()
     {
         //
